d9242efca2bd8ad86efa046226c3e41b18e7110b
ikebo <k_1043@126.com>
2021-11-12 11:12:05 +0800

    chapter 8, udp

---
+++ practice/ch8/dg_cli_traffic.c
@@ -0,0 +1,13 @@
+#include "unp.h"
+
+#define NDG 20000
+#define DGLEN 1400
+
+void dg_cli(FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen) {
+    int i;
+    char sendline[DGLEN];
+
+    for (i = 0; i < NDG; i++) {
+        Sendto(sockfd, sendline, DGLEN, 0, pservaddr, servlen);
+    }
+}
---
+++ practice/ch8/dg_echo_traffic.c
@@ -0,0 +1,25 @@
+#include "unp.h"
+
+static void recvfrom_int(int);
+static int count;
+
+void dg_echo(int sockfd, SA *pcliaddr, socklen_t clilen) {
+    socklen_t len;
+    char mesg[MAXLINE];
+
+    Signal(SIGINT, recvfrom_int);
+
+    int n = 220 * 1024;
+    Setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF, &n, sizeof(n));
+
+    for ( ; ; ) {
+        len = clilen;
+        Recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+        count++;
+    }
+}
+
+static void recvfrom_int(int signo) {
+    printf("\nreceived %d datagrams\n", count);
+    exit(0);
+}
